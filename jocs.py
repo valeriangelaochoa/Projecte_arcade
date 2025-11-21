c73be6f8540b13b5d55af30807d6f652e511b697
valeria <valeriaangelaochoa@iesmontsia.org>
2025-11-21 17:15:08 +0100

    commit2

--- jocs.py
+++ jocs.py
@@ -1,12 +1,87 @@
+from robot import robot
 def janken():
-    import robot
-    j = robot.robot()
-    for ronda in range(1,5):
-        resultat = input("¿Has guanyat aquesta ronda? (si/no): ")
+    opcions = ["pedra" "paper" "tisora"]
+    while True:
+        rondes = input("Quantes rondes vols jugar 3 o 5? ")
+        puntuacio_usuari = 0
+        puntacio_robot = 0
+        if rondes not in ["3", "5"]:
+            print("Aquesta opcio no surt en les opcions que et donem")
+        if rondes == "3":
+            while puntuacio_usuari < 4 and puntacio_robot:
+                jugada = input("Tria paper, pedra o tisora: ")
+                jugada_robot = robot().playing()
+                if jugada not in opcions:
+                    print("Aquesta elecció no esta entre les opcions per a triar")
+                elif jugada == jugada_robot:
+                    print("Han empatat")
+                elif (jugada == "pedra" and jugada_robot == "tisores"):
+                    print("Has guanyat")
+                    puntuacio_usuari += 1
+                elif (jugada == "paper" and jugada_robot == "pedra"):
+                    print("Has guanyat")
+                    puntuacio_usuari += 1
+                elif (jugada == "tisora" and jugada_robot == "paper"):
+                    print("Has guanyat")
+                    puntuacio_usuari += 1
+                elif (jugada == "pedra" and jugada_robot == "paper"):
+                    print("Ha guanyat el robot")
+                    puntacio_robot += 1
+                elif (jugada == "tisora" and jugada_robot == "pedra"):
+                    print("Ha guanyat el robot")
+                    puntacio_robot += 1
+                elif (jugada == "paper" and jugada_robot == "tisora"):
+                    print("ha guanyat el robot")
+                    puntacio_robot += 1
+                elif jugada == "S":
+                    print("Estas surtin del joc")
+                    break
+                elif puntuacio_usuari == 3:
+                    print("Has guanyat la partida")
+                    break
+                elif puntacio_robot == 3:
+                    print("Ha guanyat el robot")
+                    break
+        if rondes == "5":
+            while puntuacio_usuari < 6 and puntacio_robot:
+                jugada = input("Tria paper, pedra o tisora: ")
+                jugada_robot = robot().playing()
+                if jugada not in opcions:
+                    print("Aquesta elecció no esta entre les opcions per a triar")
+                elif jugada == jugada_robot:
+                    print("Han empatat")
+                elif (jugada == "pedra" and jugada_robot == "tisores"):
+                    print("Has guanyat")
+                    puntuacio_usuari += 1
+                elif (jugada == "paper" and jugada_robot == "pedra"):
+                    print("Has guanyat")
+                    puntuacio_usuari += 1
+                elif (jugada == "tisora" and jugada_robot == "paper"):
+                    print("Has guanyat")
+                    puntuacio_usuari += 1
+                elif (jugada == "pedra" and jugada_robot == "paper"):
+                    print("Ha guanyat el robot")
+                    puntacio_robot += 1
+                elif (jugada == "tisora" and jugada_robot == "pedra"):
+                    print("Ha guanyat el robot")
+                    puntacio_robot += 1
+                elif (jugada == "paper" and jugada_robot == "tisora"):
+                    print("ha guanyat el robot")
+                    puntacio_robot += 1
+                elif jugada == "S":
+                    print("Estas surtin del joc")
+                    break
+                elif puntuacio_usuari == 5:
+                    print("Has guanyat la partida")
+                    break
+                elif puntacio_robot == 5:
+                    print("Ha guanyat el robot")
+                    break
 
 def nana():
     import random
     numero_endevinant = random.randint(1, 100)
+    intents = 0
     while True:
         numero = int(input("Introdueix un numero: "))
         intents +=1
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import jocs
 while True:
     print("---BENVINGUT/ADA AL MINI ARCADE---")
     print("1. Jugar a Pedra, Paper, Tisora ")
     print("2. Jugar a Endevinar el Número ")
     print("S. S'ha esta tancant el mini arcade adeu....")
 
-    joc = input("Eligeix un joc o surt: ").upper()
+    choice = input("Eligeix un joc: ").upper()
+
+    match choice:
+        case "1":
+            jocs.janken()
+        case "2":
+            jocs.nana()
+        case " ":
+            print("Per favor posa una de les opcions que se le ofereix...")
+        case "S":
+            print("Moltes gracies, per jugar, s'ha esta tancat l'arcade...")
+            break
